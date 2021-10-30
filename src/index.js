b979faf298736428bec823cc4c576f086d2446a7
Femi-ige Muyiwa <84715593+muyiwexy@users.noreply.github.com>
2021-10-30 14:42:12 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var map = L.map('map').setView([9.0820, 8.6753], 7)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
